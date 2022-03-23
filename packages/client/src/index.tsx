f2648850319356802b3851c6574f8c955868d7c6
Gavyn Leavitt <firearrow5235@gmail.com>
2022-03-23 00:16:45 -0400

    Initial commit

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import { ThemeProvider, createTheme } from '@material-ui/core';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { orange } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#FF8F1F',
+    },
+    secondary: {
+      main: '#185CA1',
+    },
+    success: {
+      main: '#3F7806',
+    },
+    error: {
+      main: '#B31B1B',
+    },
+    info: {
+      main: '#38A1A1',
+    }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,5 +39,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-document.getElementsByTagName('html')[0].setAttribute("dir", "rtl");
-reportWebVitals(console.log);
+// document.getElementsByTagName('html')[0].setAttribute("dir", "rtl");
+// reportWebVitals(console.log);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { join } from 'path';
+import { App_Name } from '@portfolio/common';
 
-import { App_Name } from '@my-app/common';
 const clientPath = '../../client/build';
 const app = express();
 app.use(cors());
@@ -11,9 +11,9 @@ const port = 8080; // default port to listen
 // Serve static resources from the "public" folder (ex: when there are images to display)
 app.use(express.static(join(__dirname, clientPath)));
 
-app.get('/api', (req, res) => {
-    res.send(`Hello ${App_Name}, From server`);
-});
+// app.get('/api', (req, res) => {
+//     res.send(`Hello ${App_Name}, From server`);
+// });
 
 // Serve the HTML page
 app.get('*', (req: any, res: any) => {
